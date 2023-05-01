a6a3a8715a468709c922db3a03d47f63fb5cb9b1
ajaythoram <115677014+ajaythoram@users.noreply.github.com>
2023-05-01 08:47:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const colorSelect = document.querySelector('#colorSelect');
+const removeColorButton = document.querySelector('#removeColorButton');
+
+removeColorButton.addEventListener('click', () => {
+  const selectedOption = colorSelect.options[colorSelect.selectedIndex];
+  const colorToRemove = selectedOption.value;
+
+  // Remove the selected color from the dropdown list
+  for (let i = 0; i < colorSelect.options.length; i++) {
+    const option = colorSelect.options[i];
+    if (option.value === colorToRemove) {
+      colorSelect.removeChild(option);
+      break;
+    }
+  }
+});
+
